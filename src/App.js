59d2c7a624e69878e713d30d841f5bc204c40174
omolo-j <omoloj135@gmail.com>
2024-05-07 18:16:18 +0300

    import navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div>
-      
+      <header>
+        <NavBar />
+      </header>
     </div>
   )
 }
