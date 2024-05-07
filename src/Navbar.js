b67e8e923c43bd789653cd94d7b5ba946818d91a
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-05-07 22:23:25 +0300

    css

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './Navbar.css';
+import './App.css';
+
 
 function Navbar() {
     return (
         <nav>
-            <ul>
+            <ul className="horizontal-menu"> 
                 <li><a href="#home">Home</a></li>
                 <li><a href="#menu">Menu</a></li>
                 <li><a href="#about">About Us</a></li>
