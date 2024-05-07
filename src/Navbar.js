57c0fc02ff930b717c3804ea38fad2c1850b0a06
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-05-07 19:15:39 +0300

    Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#about">About Us</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
