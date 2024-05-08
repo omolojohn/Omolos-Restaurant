4bd5a616a7ee307b9875d697d3fffb43ff5f301a
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-05-08 08:29:02 +0300

    menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavBar from './Navbar';
-
+import Menu from './Menu'
 function App() {
   return (
     <div>
---
+++ src/Menu.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Menu = () => {
+    return (
+        <div className="container menu-parent-div text-center">
+            <div className="animate-text menu-header">
+                <div className="font-weight-bold header-bold" style={{ fontSize: "20px", color: "#4c4c4c"}}>
+                    <h1 style={{ color: "#152f4e"}}>Welcome to Omolos Restaurant</h1>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
