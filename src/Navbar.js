e515f965d476d5506cb8fedb5c16e9eb24bca61f
omolo-j <omoloj135@gmail.com>
2024-05-08 07:47:46 +0300

    style update

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css';
 function Navbar() {
     return (
         <nav>
-            <ul>
+            <ul className='horizontal-menu'>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#menu">Menu</a></li>
                 <li><a href="#about">About Us</a></li>
