cff64eeadc227145aaa6804ca61cba7b63688569
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-05-08 11:46:16 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import NavBar from './Navbar';
-import Menu from "./Menu"
+import { BrowserRouter as Router } from "react-router-dom";
+
+import "./index.css";
+import Navbar from "./Navbar";
+import "./App.css";
+import './App.css'
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-      <header>
-        <NavBar />
-        <Menu />
-      </header>
-    </div>
-  )
-}
+    <Router>
+      <div>
+     <Navbar />
+      
+        <Routes>
 
+          
+         
+          <Route path="*" element={<h1>404 not found</h1>} />
+          </Routes>
+          
+    </div>
+  </Router>  
+  );
+}
 export default App;
