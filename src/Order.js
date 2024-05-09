46f5054ef98ae0399ea5ccf8eccfa9e2b4fbff38
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-05-09 10:46:55 +0300

    errors

---
+++ src/Order.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function Order() {
+  const [orderVisible, setOrderVisible] = useState(false);
+
+  const toggleOrder = () => {
+    setOrderVisible(!orderVisible);
+  };
+
+  return (
+
+    
+      <div>
+        <ul onClick={toggleOrder}><button>Order</button></ul>
+        {orderVisible && (
+          <form>
+            <label>Name:</label>
+            <input type="text" />
+            <label>Email:</label>
+            <input type="email" />
+            <label>Phone:</label>
+            <input type="tel" />
+            <button type="submit">Place Order</button>
+          </form>
+        )}
+      </div>
+  );
+}
+
+export default Order;
