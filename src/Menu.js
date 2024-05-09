ac9abd1deebbc82560e782c3b975b04c1067c5ac
omolo-j <omoloj135@gmail.com>
2024-05-09 10:45:06 +0300

    changes in the order component

--- src/Menu.js
+++ src/Menu.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-
+import { isHtmlElement } from 'react-router-dom/dist/dom';
+import './App.css'
 function Menu({ onSelect }) {
     const [dishes, setDishes] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:3000/menu')
+        fetch('http://localhost:3001/menu')
             .then(response => response.json())
             .then(data => setDishes(data))
             .catch(error => console.error('Error fetching dishes:', error));
@@ -12,18 +13,18 @@ function Menu({ onSelect }) {
 
     const handleSelectItem = (item) => {
         onSelect(item);
-    };
-
+    }
     return (
         <div className='menu'>
             <h1>Our Menu</h1>
             <div>
                 {dishes.map(dish => (
                     <div key={dish.id} onClick={() => handleSelectItem(dish)}>
-                         <img src={dish.image_url} alt={dish.name} />
+                        <img src={dish.image} alt={dish.name} />
                         <h2>{dish.name}</h2>
                         <p>{dish.description}</p>
-                        <p>{dish.price}</p>
+                         <p>{dish.price}</p>
+        
                     </div>
                 ))}
             </div>
