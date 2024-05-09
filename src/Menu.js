6a20fd3ebe747b600b340b236ca955b87677a121
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-05-09 12:48:48 +0300

    menu

--- src/Menu.js
+++ src/Menu.js
@@ -3,6 +3,7 @@ import './App.css'
 
 function Menu() {
     const [dishes, setDishes] = useState([]);
+    const [ selectedDish, setSelectedDish] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:3000/menu')
@@ -11,12 +12,18 @@ function Menu() {
             .catch(error => console.error('Error fetching dishes:', error));
     }, []);
 
+    const handleDishClick = (dish) => {
+        setSelectedDish(dish);
+
+        console.log("Selected dish:", dish);
+    };
+
     return (
         <div>
             <h1>Our Menu</h1>
             <div>
                 {dishes.map(dish => (
-                    <div key={dish.id}>
+                 <div key={dish.id} onClick={() => handleDishClick(dish)}>   
                         <img src={dish.image} alt={dish.name} />
                         <h2>{dish.name}</h2>
                         <p>{dish.description}</p>
